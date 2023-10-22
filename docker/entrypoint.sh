c5a4255e5e0c5cb352181e56556fb2d069b7c527
halukbuyukozkanü <halukbuyukozkan@gmail.com>
2023-10-22 10:48:17 +0300

    php service and commands created

---
+++ docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -f vendor/autoload.php ]; then 
+    compsoer install --no-progress --no-interaction
+fi
+
+if [ ! -f .env ]; then 
+    echo "Creating .env file for env $APP_ENV"
+    cp .env.example .env
+else
+    echo ".env file already exists"
+fi
+
+php artisan migrate
+php artisan key:generate
+php artisan optimize:clear
+
+php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+exec docker-php-entrypoint "$@"
