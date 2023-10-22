7e63f2e12b24bb79d253f41eed915eeb8f6682e2
halukbuyukozkanü <halukbuyukozkan@gmail.com>
2023-10-22 18:42:35 +0300

    env example configured, node server established

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,9 +11,22 @@ else
     echo ".env file already exists"
 fi
 
-php artisan migrate
-php artisan key:generate
-php artisan optimize:clear
+role=${CONTAINER_ROLE:-app}
 
-php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+if [ "$role" = "app" ]; then
+    php artisan migrate
+    php artisan key:generate
+    php artisan cache:clear
+    php artisan config:clear
+    php artisan route:clear
+    php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+elif [ "$role" = "queue" ]; then
+    echo "Running the queue"
+    php /var/www/artisan queue:work --verbose --tries=3 --timeout=90
+else
+    echo "Could not match the container role \"$role\""
+    exit 1
+fi
+
+
 exec docker-php-entrypoint "$@"
